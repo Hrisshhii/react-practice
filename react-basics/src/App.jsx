d5e7ea38ab75791eee0080bd1caf616b363908f2
Hrishi <rathodhrishi12345@gmail.com>
2026-01-14 23:04:09 +0530

    more about useEffect

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,27 +1,23 @@
 import './App.css';
 import { useState,useEffect } from 'react';
 function App() {
-  const [value,setValue]=useState(0);
-  const [something,setSome]=useState(0);
+  const [data,setData]=useState([]);
   useEffect(()=>{
-    console.log('Call useEffect');
-    document.title=`Increment ${value}`;
-  },[value]);
-// empty dependency array means only load once
-// if none then it load everytime 
-// if [value] anytime change in component fire function
-
-  useEffect(()=>{
-    console.log('Call useEffect using Something');
-    document.title=`Increment ${something}`;
-  },[something]);
+    async function getData() {
+      const result=await fetch("https://jsonplaceholder.typicode.com/todos");
+      const data=await result.json();
+      if (data && data.length) setData(data)
+    }
+  getData();
+  },[]);
 
   return (
     <>
-      <h2>value: {value}</h2>
-      <button onClick={()=>setValue(value+1)}>Increment</button>
-      <h2>Value by something: {something}</h2>
-      <button onClick={()=>setSome(something+1)}>Increment by something</button>
+      <ul>
+        {data.map((todo)=>(
+          <li key={crypto.randomUUID()}>{todo.title}</li>
+        ))}
+      </ul>
     </>
   )
 }
