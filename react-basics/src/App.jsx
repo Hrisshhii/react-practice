6fa6fc99d1dbb153a4e990802d95cbeed5d7673c
Hrishi <rathodhrishi12345@gmail.com>
2026-01-14 22:53:22 +0530

    useEffect basics practice

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,13 +1,27 @@
 import './App.css';
-import CopyInput from './components/CopyInput';
-import { Counter, ToDoList } from './components/Mini';
-import { Switcher } from './components/Switcher';
+import { useState,useEffect } from 'react';
 function App() {
+  const [value,setValue]=useState(0);
+  const [something,setSome]=useState(0);
+  useEffect(()=>{
+    console.log('Call useEffect');
+    document.title=`Increment ${value}`;
+  },[value]);
+// empty dependency array means only load once
+// if none then it load everytime 
+// if [value] anytime change in component fire function
+
+  useEffect(()=>{
+    console.log('Call useEffect using Something');
+    document.title=`Increment ${something}`;
+  },[something]);
+
   return (
     <>
-      <CopyInput />
-      <br></br>
-      <Switcher />
+      <h2>value: {value}</h2>
+      <button onClick={()=>setValue(value+1)}>Increment</button>
+      <h2>Value by something: {something}</h2>
+      <button onClick={()=>setSome(something+1)}>Increment by something</button>
     </>
   )
 }
