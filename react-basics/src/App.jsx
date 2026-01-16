d0007b2dbfa46b9a75ec2631138ba96bc986cd49
Hrishi <rathodhrishi12345@gmail.com>
2026-01-16 23:26:37 +0530

    UseRef hook basics

--- react-basics/src/App.jsx
+++ react-basics/src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css';
-import { UseReducer } from './components/UseReducer';
+import { useRef } from 'react';
 
 function App() {
-  
+  const inputElem=useRef(null);
+  function focusInput(){
+    inputElem.current.focus();
+    inputElem.current.value="Hello";
+  };
   return (
-    <UseReducer />
+    <>
+      <input type="text" ref={inputElem}/>
+      <button onClick={()=>focusInput()}>Focus Element</button>
+    </>
   );
 }
 
