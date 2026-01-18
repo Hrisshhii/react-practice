14b77560374b5db323c9d728835066312cf7b0dd
Hrishi <rathodhrishi12345@gmail.com>
2026-01-19 01:57:27 +0530

    Routing and Registeration for Testimonials miniproject

---
+++ MiniProjects/src/components/Testimonials/Testimonials.jsx
@@ -0,0 +1,10 @@
+import { BackHome } from '../BackHome';
+import './Testimonials.css';
+
+export function Testimonials(){
+    return (
+        <div className='testimonials'>
+            <BackHome />
+        </div>
+    );
+};
