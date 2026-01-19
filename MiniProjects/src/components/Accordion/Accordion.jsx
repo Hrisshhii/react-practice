08d25951c16593b35f0bf48c959165ec384ba47d
Hrishi <rathodhrishi12345@gmail.com>
2026-01-19 11:46:27 +0530

    Adding keyDown to Accordion project

--- MiniProjects/src/components/Accordion/Accordion.jsx
+++ MiniProjects/src/components/Accordion/Accordion.jsx
@@ -9,17 +9,32 @@ export function Accordion(){
         setActiveId(prev=>(prev===id? null : id));
     };
 
+    const keyDown=(e,i,id)=>{
+        if(e.key==="Enter" || e.key===" " || e.key==="Space"){
+            e.preventDefault();
+            toggle(id);
+        }
+        if(e.key==="ArrowDown"){
+            e.preventDefault();
+            document.querySelectorAll(".accordion-header")[i+1]?.focus();
+        }
+        if(e.key==="ArrowUp"){
+            e.preventDefault();
+            document.querySelectorAll(".accordion-header")[i-1]?.focus();
+        }
+    };
+
     return (
         <div className='accordion-page'>
             <BackHome />
             <div className='accordion'>
                 <h1 className='accordion-title'>📂 Accordion</h1>
-                {accordionData.map(item=>{
+                {accordionData.map((item,index)=>{
                     const isActive= activeId===item.id;
                     
                     return (
                         <div key={item.id} className={`accordion-item ${activeId===item.id ? "open":""}`}>
-                            <button className='accordion-header' onClick={()=>toggle(item.id)}>
+                            <button className='accordion-header' onClick={()=>toggle(item.id)} onKeyDown={(e)=>keyDown(e,index,item.id)}>
                                 <span>{item.title}</span>
                                 <span className='icon'>{activeId === item.id ? "-": "+"}</span>
                             </button>
