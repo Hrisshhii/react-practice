9d53f22f97c55f13965e257c36c1ebf1358e1408
Hrishi <rathodhrishi12345@gmail.com>
2026-01-22 20:58:44 +0530

    feat: add multi-dimensional array example and display in Arrays component

--- Typescript-practice/src/Basics/Arrays.tsx
+++ Typescript-practice/src/Basics/Arrays.tsx
@@ -12,10 +12,21 @@ console.log(components);
 components.push('Mouse');
 console.log(components);
 
+//Multi-dimensional Array
+const matrix: number[][]=[
+    [1, 2, 3],
+    [10, 11, 12]
+];
+console.log(matrix[1][2]);
+console.log(matrix[0][1]);
+console.log(matrix);
+
+
 export function Arrays(){
     return <>
         <p>{nums}</p>
         <p>{items}</p>
         <p>{components}</p>
+        <p>{matrix}</p>
     </>
 };
