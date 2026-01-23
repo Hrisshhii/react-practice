d6b67f368b8a538b674a928f6f9e512057d869aa
Hrishi <rathodhrishi12345@gmail.com>
2026-01-23 22:00:39 +0530

    Interface implemention

--- Typescript-practice/src/Basics/Interfaces.tsx
+++ Typescript-practice/src/Basics/Interfaces.tsx
@@ -5,6 +5,7 @@ interface Person{
     age: number;
     greet():string;
 }
+
 class P1 implements Person{
     name: string="Hugh Jackman";
     age: number=52;
@@ -15,8 +16,19 @@ class P1 implements Person{
 const p1=new P1();
 console.log(p1.greet());
 
+const p2: Person={
+    name: "Ryan Reynolds",
+    age: 47,
+    greet(): string{
+        return `Hello ${this.name},age is ${this.age}`;
+    }
+};
+console.log(p2.greet());
 export const Interfaces = () => {
   return (
-    <div>{p1.greet()}</div>
+    <div>
+        <p>{p1.greet()}</p>
+        <p>{p2.greet()}</p>
+    </div>
   )
 }
