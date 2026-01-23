6519a3e1891f70245fd831a8defa1ac8efc8846a
Hrishi <rathodhrishi12345@gmail.com>
2026-01-23 21:33:16 +0530

    Setters and getters

--- Typescript-practice/src/Basics/ClassOOP.tsx
+++ Typescript-practice/src/Basics/ClassOOP.tsx
@@ -8,16 +8,43 @@ class Person{
         this.name=name;
         this.age=age;
     }
+    getName():string{
+        return this.name;
+    }
 };
+
+//extends for inheritance
+//syntax: class ChildClass extends className{}
 
 const person=new Person('Alex',25);
 console.log(typeof person);
 
+//Getters and Setters
+//getters are ued to get private properties
+//setters used to set private properties
+
+class Employee{
+  private empName: string="John Doe";
+
+  get myProperty(): string{
+    return this.empName;
+  }
+
+  set myProperty(value: string){
+      this.empName=value;
+  }
+}
+
+const emp=new Employee();
+emp.myProperty="Ryan Gosling";
+console.log(emp.myProperty);
+
 const ClassOOP = () => {
   return (
     <div>
-        <p>Name: {person.name}</p>
+        <p>Name: {person.getName()}</p>
         <p>Age: {person.age}</p>
+        <p>Employee Name: {emp.myProperty}</p>
     </div>
   )
 }
