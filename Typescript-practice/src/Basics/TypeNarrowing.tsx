29da348bc3652ffff6526ab40b6b5fddd3ef45b3
Hrishi <rathodhrishi12345@gmail.com>
2026-01-24 18:47:45 +0530

    Type Narrowing in TypeScript

--- Typescript-practice/src/Basics/TypeNarrowing.tsx
+++ Typescript-practice/src/Basics/TypeNarrowing.tsx
@@ -1,21 +1,23 @@
 // Type Narrowing in TypeScript
 type MyType= string | number;
 
-function exampleFun(val: MyType):void{
+function exampleFun(val: MyType):MyType{
     if(typeof val==="string"){
         console.log(val.toUpperCase());
+        return val.toUpperCase();
     }else{
         console.log(val.toFixed(2));
+        return val.toFixed(2);
     }
 }
 
-exampleFun("hello");
-exampleFun(11);
 
 export const TypeNarrowing = () => {
   return (
     <div>
         <h2>Type Narrowing</h2>
+        <p>{exampleFun("hello")}</p>
+        <p>{exampleFun(11)}</p>
     </div>
   )
 }
