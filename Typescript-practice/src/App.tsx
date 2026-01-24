a3bcf816e711cc438b1537d82a0d1302d30b8ff1
Hrishi <rathodhrishi12345@gmail.com>
2026-01-24 20:16:21 +0530

    Setting up react typescript

--- Typescript-practice/src/App.tsx
+++ Typescript-practice/src/App.tsx
@@ -1,20 +1,21 @@
-import './App.css'
-import { Annotations } from './Basics/Annotations'
-import { Arrays } from './Basics/Arrays'
-import ClassOOP from './Basics/ClassOOP'
-import { Interfaces } from './Basics/Interfaces'
-import { Objects } from './Basics/Objects'
-import { TypeNarrowing } from './Basics/TypeNarrowing'
 
+import Users from './Components/Users'
+// import { Annotations } from './Basics/Annotations'
+// import { Arrays } from './Basics/Arrays'
+// import ClassOOP from './Basics/ClassOOP'
+// import { Interfaces } from './Basics/Interfaces'
+// import { Objects } from './Basics/Objects'
+// import { TypeNarrowing } from './Basics/TypeNarrowing'
 function App() {
   return (
     <>
-      {<Annotations />}
+      {/* {<Annotations />}
       {<Arrays />}
       {<Objects />}
       {<ClassOOP />}
       {<Interfaces />}
-      {<TypeNarrowing />}
+      {<TypeNarrowing />} */}
+      <Users />
     </>
   )
 }
