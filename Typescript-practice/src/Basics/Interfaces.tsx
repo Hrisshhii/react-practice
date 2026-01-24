e02b0a2501ec4b9bcda31387d73d522ed4a034e6
Hrishi <rathodhrishi12345@gmail.com>
2026-01-24 16:42:07 +0530

    Generic key value pair from object

--- Typescript-practice/src/Basics/Interfaces.tsx
+++ Typescript-practice/src/Basics/Interfaces.tsx
@@ -137,6 +137,25 @@ const num=printInfo<number>(2);
 console.log(str);
 console.log(num);
 
+//Generic key value pair from object
+
+function getRandomKeyValuePair<T>(obj:{[key:string]: T}):{
+    key:string;
+    value:T;
+}{
+    const keys=Object.keys(obj);
+    const randomKey=keys[Math.floor(Math.random()*keys.length)];
+    return {key: randomKey,value: obj[randomKey]};
+}
+
+const strObj={a:"Apple",b:"Banana",c:"Cherry"}
+const res=getRandomKeyValuePair<string>(strObj);
+console.log(res);
+
+const numObj={one:1,two:2,three:3};
+const resNum=getRandomKeyValuePair<number>(numObj);
+console.log(resNum);
+
 //Generics with intefaces 
 interface Pair<T,U>{
     first : T;
