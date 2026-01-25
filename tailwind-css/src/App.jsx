7f9357a5d1cbd73e53da0d491a87e70e5fcd0cbc
Hrishi <rathodhrishi12345@gmail.com>
2026-01-26 00:38:56 +0530

    Tailwind base setup

---
+++ tailwind-css/src/App.jsx
@@ -0,0 +1,20 @@
+import './index.css'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-black p-6">
+        <div className="flex space-x-12">
+          <a className="text-sm font-semibold text-white">Product</a>
+          <a className="text-sm font-semibold text-white">Features</a>
+          <a className="text-sm font-semibold text-white">Marketplace</a>
+          <a className="text-sm font-semibold text-white">Company</a>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
