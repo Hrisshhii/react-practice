bd017c5bdc7e75fde95ac310222dcab6d0cec359
Hrishi <rathodhrishi12345@gmail.com>
2026-01-28 19:31:24 +0530

    Sidebar functionalities

--- ts-miniprojects/src/Components/Tabs Project/Sidebar.tsx
+++ ts-miniprojects/src/Components/Tabs Project/Sidebar.tsx
@@ -3,25 +3,33 @@ import { BackHome } from "../BackHome";
 import { IoMdSettings } from "react-icons/io";
 import { MdOutlineArrowBackIos } from "react-icons/md";
 
-const Sidebar = () => {
+type Props={
+  isActive:"profile"|"inventory"|"settings";
+  setIsActive:(tab: "profile"|"inventory"|"settings")=>void;
+};
+
+const Sidebar = ({isActive,setIsActive}:Props) => {
+  const iconClass=(tab:string)=>
+    `cursor-pointer transition ${
+      isActive===tab?"text-blue-400 scale-110":"opacity-70 hover:opacity-100"
+    }`;
   return (
     <aside className="fixed top-0 left-0 h-screen w-16 md:w-20 bg-[#1A1C1E] text-white flex flex-col items-center py-4">
       
       {/* Top Section */}
       <div className="flex flex-col items-center gap-6">
         <BackHome />
-        <MdOutlineArrowBackIos size={22} className="opacity-80 hover:opacity-100 transition" />
-        <FaUser size={22} className="opacity-80 hover:opacity-100 transition" />
-        <FaSearch size={22} className="opacity-80 hover:opacity-100 transition" />
+        <MdOutlineArrowBackIos size={22} className="opacity-80 hover:opacity-100 transition" onClick={()=>setIsActive("profile")}/>
+        <FaUser size={22} className={iconClass("profile")} onClick={()=>setIsActive("profile")}/>
+        <FaSearch size={22} className={iconClass("inventory")} onClick={()=>setIsActive("inventory")} />
       </div>
 
       {/* Pushes bottom section down but keeps inside screen */}
       <div className="grow" />
 
       {/* Bottom Section */}
-      <div className="flex flex-col items-center gap-6 pb-4">
-        <IoMdSettings size={22} className="opacity-80 hover:opacity-100 transition" />
-        <FaUser size={22} className="opacity-80 hover:opacity-100 transition mb-2" />
+      <div className="flex flex-col items-center gap-6 pb-4 mb-4">
+        <IoMdSettings size={22} className={iconClass("settings")} onClick={()=>setIsActive("settings")} />
       </div>
 
     </aside>
--- ts-miniprojects/src/Components/Tabs Project/Tabs.tsx
+++ ts-miniprojects/src/Components/Tabs Project/Tabs.tsx
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import { Profile } from "./Profile";
 import Sidebar from "./Sidebar";
 
+type Tab="profile"|"inventory"|"settings";
+
 const Tabs = () => {
+  const  [isActive,setIsActive]=useState<Tab>('profile');
   return (
     <div className="flex">
-      <Sidebar />
-      <Profile />
+      <Sidebar isActive={isActive} setIsActive={setIsActive}/>
+      {isActive==="profile" && <Profile />}
     </div>
   );
 };
