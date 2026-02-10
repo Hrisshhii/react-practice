9d96c409babcc24e46b5bfdd45e049d59f813c6c
Hrishi <rathodhrishi12345@gmail.com>
2026-02-10 23:50:35 +0530

    color changes to button

--- ts-miniprojects/src/Components/Project Tracker Project/ControlBar.tsx
+++ ts-miniprojects/src/Components/Project Tracker Project/ControlBar.tsx
@@ -8,7 +8,7 @@ type Props={
 
 const ControlBar = ({search,setSearch,openCreate}:Props) => {
   return (
-    <div className="flex items-center justify-between px-8 py-5 border-b border-white/5">
+    <div className="flex items-center justify-between px-8 py-5 border-b border-white/30">
       <div className="flex items-center justify-between w-full">
           <input type="text" placeholder="Search project..."
            value={search}
@@ -18,7 +18,11 @@ const ControlBar = ({search,setSearch,openCreate}:Props) => {
         <div className="relative flex justify-center items-center gap-4">
           <button className="text-[1.12rem] rounded bg-transparent text-white hover:bg-white/10 cursor-pointer py-3 px-8 border-solid border-gray-800">Filter</button>
           <button className="text-[1.12rem] rounded bg-transparent text-white hover:bg-white/10 cursor-pointer py-3 px-8 border-solid border-gray-800">Sort</button>
-          <button onClick={openCreate} className="bg-[#b1cbe2] text-black text-3xl w-14 h-14 rounded-full shadow-lg hover:scale-110 transition cursor-pointer">+</button>
+          <button onClick={openCreate} 
+           className="bg-[#b1cbe2] text-black text-3xl w-14 h-14 rounded-full shadow-lg hover:scale-110 transition cursor-pointer hover:bg-[#b1cbe2]/50"
+          >
+            +
+          </button>
         </div>
       </div>
     </div>
