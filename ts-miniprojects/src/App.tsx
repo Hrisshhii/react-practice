8320222c03033b9ee1d474970967e5d83fbd20ad
Hrishi <rathodhrishi12345@gmail.com>
2026-02-10 22:45:33 +0530

    Starting with the project tracter project with components main page

--- ts-miniprojects/src/App.tsx
+++ ts-miniprojects/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import HomePage from './Components/HomePage'
 import Tabs from './Components/Tabs Project/Tabs'
 import Blogs from './Components/Blogs Project/Blogs'
+import ProjectTracker from './Components/Project Tracker Project/ProjectTracker'
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Route path="/" element={<HomePage />} />
       <Route path="/tabs" element={<Tabs />} />
       <Route path="/blogs" element={<Blogs />}/>
-      <Route path="/project-tracker" element={''}/>
+      <Route path="/project-tracker" element={<ProjectTracker/>}/>
     </Routes>
   )
 }
---
+++ ts-miniprojects/src/Components/Project Tracker Project/ProjectTracker.tsx
@@ -0,0 +1,10 @@
+
+const ProjectTracker = () => {
+  return (
+    <div className="min-h-screen bg-[#121212]">
+      
+    </div>
+  )
+}
+
+export default ProjectTracker
