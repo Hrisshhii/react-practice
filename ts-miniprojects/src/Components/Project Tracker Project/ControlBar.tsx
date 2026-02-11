86ce640afcbc39d11150f51a745d32edb32eb611
Hrishi <rathodhrishi12345@gmail.com>
2026-02-11 21:05:46 +0530

    Fixing mobile responsivenss

--- ts-miniprojects/src/Components/Project Tracker Project/ControlBar.tsx
+++ ts-miniprojects/src/Components/Project Tracker Project/ControlBar.tsx
@@ -8,18 +8,18 @@ type Props={
 
 const ControlBar = ({search,setSearch,openCreate}:Props) => {
   return (
-    <div className="flex items-center justify-between px-8 py-5 border-b border-white/30">
-      <div className="flex items-center justify-between w-full">
-          <input type="text" placeholder="Search project..."
+    <div className="flex items-center justify-between px-4 sm:px-8 py-5 border-b border-white/30">
+      <div className="flex flex-col sm:flex-row items-center sm:justify-between w-full gap-4">
+          <input type="text" placeholder="Search project . . ."
            value={search}
            onChange={e=>setSearch(e.target.value)}
-           className="bg-transparent text-white border border-solid rounded-full border-gray-600 text-[1.12rem] p-5 w-[40%]"
+           className="bg-transparent text-white border border-solid rounded-full border-gray-600 text-[1.12rem] p-5 w-[80%] sm:w-[40%]"
           />
-        <div className="relative flex justify-center items-center gap-4">
-          <button className="text-[1.12rem] rounded bg-transparent text-white hover:bg-white/10 cursor-pointer py-3 px-8 border-solid border-gray-800">Filter</button>
-          <button className="text-[1.12rem] rounded bg-transparent text-white hover:bg-white/10 cursor-pointer py-3 px-8 border-solid border-gray-800">Sort</button>
+        <div className="relative flex justify-center items-center gap-8 sm:gap-4">
+          <button className="text-[0.95rem] sm:text-[1.12rem] rounded bg-transparent text-white hover:bg-white/10 cursor-pointer py-2 px-6 sm:py-3 sm:px-8 border-solid border-gray-800">Filter</button>
+          <button className="text-[0.95rem] sm:text-[1.12rem] rounded bg-transparent text-white hover:bg-white/10 cursor-pointer py-2 px-6 sm:py-3 sm:px-8 border-solid border-gray-800">Sort</button>
           <button onClick={openCreate} 
-           className="bg-[#b1cbe2] text-black text-3xl w-14 h-14 rounded-full shadow-lg hover:scale-110 transition cursor-pointer hover:bg-[#b1cbe2]/50"
+           className="bg-[#b1cbe2] text-black text-xl sm:text-3xl w-10 h-10 sm:w-14 sm:h-14 rounded-full shadow-lg hover:scale-110 transition cursor-pointer hover:bg-[#b1cbe2]/50"
           >
             +
           </button>
--- ts-miniprojects/src/Components/Project Tracker Project/Navigation.tsx
+++ ts-miniprojects/src/Components/Project Tracker Project/Navigation.tsx
@@ -4,9 +4,12 @@ import { IoMdSettings } from "react-icons/io"
 
 const Navigation = () => {
   return (
-    <div className="flex justify-between items-center bg-[#1A1C1E] p-4">
+    <div className="relative flex justify-between items-center bg-[#1A1C1E] p-4">
       <BackHome/>
-      <div className="flex justify-between items-center gap-5">
+      <h1 className="hidden md:block h1-title text-center">
+        Project Tracker
+      </h1>
+      <div className="flex items-center gap-5">
         <IoMdSettings className="text-gray-600 hover:text-gray-400 cursor-pointer" size={28}/>
         <FaHome className="text-gray-600 hover:text-gray-400 cursor-pointer" size={28}/>
         <FaUserAstronaut className="text-gray-600 hover:text-gray-400 cursor-pointer" size={28}/>
