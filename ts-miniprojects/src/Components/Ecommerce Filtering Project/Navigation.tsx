a44ce7d7c2699612a787d184045a08455f9c1e2b
Hrishi <rathodhrishi12345@gmail.com>
2026-02-15 01:22:34 +0530

    Completing the navigation css and transitions

--- ts-miniprojects/src/Components/Ecommerce Filtering Project/Navigation.tsx
+++ ts-miniprojects/src/Components/Ecommerce Filtering Project/Navigation.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react"
 import { BackHome } from "../BackHome"
 import { MdOutlineArrowBackIos } from "react-icons/md";
 import { IoMdSettings } from "react-icons/io";
+import { FaUser } from "react-icons/fa";
 
 interface Products{
   id: number;
@@ -15,7 +16,7 @@ interface FetchResponse{
 
 const Navigation = () => {
   const [categories,setCategories]=useState<string[]>([]);
-  const [showNavBar,setShowNavbar]=useState<boolean>(true);
+  const [showNavBar,setShowNavbar]=useState<boolean>(false);
   const [keyword]=useState<string[]>(
     ["apple","watch","fashion","trend","shoes","shirts"]
   );
@@ -37,39 +38,53 @@ const Navigation = () => {
   },[]);
 
   return (
-    <aside className={`flex flex-col justify-between p-3 min-h-screen ${showNavBar?"bg-[#1A1C1E] w-[10vw]":"w-[2vw] bg-[#1A1C1E]"}`}>
-      <BackHome />
-      <div className={`${showNavBar?"text-center":""}`}>
-        <MdOutlineArrowBackIos onClick={()=>setShowNavbar(!showNavBar)} size={24} 
-          className={`text-white/80 hover:text-white/40 cursor-pointer p-1 
-          ${showNavBar?"":"rotate-180"}`}
-        />
-      </div>
-      
-      <div className={`p-2 min-h-screen transition ${showNavBar?"":"hidden"}`}>
-        <div className="flex flex-col gap-3 ">
-          <input type="text" placeholder="Search Items" className="text-[1.2rem] px-3 py-2 text-white/65 bg-transparent border-solid rounded-full focus:outline-none"/>
-          <div className="grid grid-cols-2 gap-1.5">
-            <input type="text" placeholder="Max" className="text-[1.2rem] px-3 py-2 text-white/65 bg-transparent border-solid rounded-md focus:outline-none"/>
-            <input type="text" placeholder="Min" className="text-[1.2rem] px-3 py-2 text-white/65 bg-transparent border-solid rounded-md focus:outline-none"/>
-          </div>
-          <div className="border-x-2 border-white/25">
-            <h2 className="text-center text-white/75">Categories</h2>
-          </div>
-          {categories.map(cat=>(
-            <button key={cat} className="text-xs md:text-sm bg-white/5 hover:bg-white/10 rounded-md py-2 px-3 border-solid border-white/10 transition text-white cursor-pointer">{cat}</button>
-          ))}
-          <div className="border-x-2 border-white/25">
-            <h2 className="text-center text-white/75">Keywords</h2>
-          </div>
-          {keyword.map((word)=>(
-            <button key={word} className="text-xs md:text-sm bg-white/5 hover:bg-white/10 rounded-md py-2 px-3 border-solid border-white/10 transition text-white cursor-pointer">{word}</button>
-          ))}
-          <button className="mt-3 text-s md:text-sm bg-black/50 hover:bg-black/10 rounded-md py-3 px-3 border-solid border-white/30 transition text-white cursor-pointer">Reset Filters</button>
+    <aside className={`flex flex-col transition-[width] duration-500 font-mono h-screen ${showNavBar?"bg-[#1A1C1E] w-64":"w-16 bg-[#1A1C1E] justify-between"}`}>
+      <div className="px-3 pb-2 pt-6">
+        <BackHome />
+        <div className={`text-center pt-4`}>
+          <MdOutlineArrowBackIos onClick={()=>setShowNavbar(!showNavBar)} size={24} 
+            className={`text-white/80 hover:text-white/40 cursor-pointer 
+            ${showNavBar?"":"rotate-180"}`}
+          />
         </div>
       </div>
-      <div  className="text-center">
+      
+      <div className={`flex-1 overflow-y-auto overflow-x-hidden scrollbar-hidden px-3 transition ${showNavBar?"":"hidden"}`}>
+        <div className="flex flex-col gap-2 ">
+          <input type="text" placeholder="Search Items . . . " className="text-[1.15rem] px-3 py-3 text-white/65 bg-transparent border-solid rounded-full focus:outline-none"/>
+          <div className="grid grid-cols-2 gap-1.5">
+            <input type="text" placeholder="Max" className="text-[1.1rem] px-3 py-1.5 text-white/65 bg-transparent border-solid rounded-lg focus:outline-none"/>
+            <input type="text" placeholder="Min" className="text-[1.1rem] px-3 py-1.5 text-white/65 bg-transparent border-solid rounded-lg focus:outline-none"/>
+          </div>
+          <div>
+            <h2 className="text-center text-[#bbc9ee]">Categories</h2>
+          </div>
+          {categories.map(cat => (
+            <label key={cat} className="flex items-center justify-between px-3 py-2 rounded-md cursor-pointer hover:bg-white/5 transition">
+              <span className="text-[1.2rem] text-white/75 capitalize">
+                {cat}
+              </span>
+
+              <input
+                type="radio"
+                name="category"
+                value={cat}
+                className="accent-blue-400 cursor-pointer"
+              />
+            </label>
+          ))}
+          <div>
+            <h2 className="text-center text-[#bbc9ee]">Keywords</h2>
+          </div>
+          {keyword.map((word)=>(
+            <button key={word} className="font-mono text-xs md:text-sm bg-white/5 hover:bg-white/10 rounded-md py-2 px-3 border-solid border-white/10 transition text-white cursor-pointer">{word}</button>
+          ))}
+          <button className="font-mono mt-3 text-sm text-semibold bg-black/50 hover:bg-black/10 rounded-md py-3 px-3 border-solid border-white/30 transition text-white cursor-pointer">Reset Filters</button>
+        </div>
+      </div>
+      <div className={`px-3 py-4 flex flex-col items-center gap-4`}>
         <IoMdSettings size={24} className="text-white/80 hover:text-white/40 cursor-pointer p-1 "/>
+        <FaUser size={24} className="text-white/80 hover:text-white/40 cursor-pointer p-1 "/>
       </div>
     </aside>
   )
