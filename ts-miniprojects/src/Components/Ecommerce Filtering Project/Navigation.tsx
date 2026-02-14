f8df2a7db442f2517d93a83416aba5110ebe32dd
Hrishi <rathodhrishi12345@gmail.com>
2026-02-15 02:09:20 +0530

    Styling upgrades for nav Bar

--- ts-miniprojects/src/Components/Ecommerce Filtering Project/Navigation.tsx
+++ ts-miniprojects/src/Components/Ecommerce Filtering Project/Navigation.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 import { BackHome } from "../BackHome"
-import { MdOutlineArrowBackIos } from "react-icons/md";
+import { MdOutlineArrowBackIos, MdOutlineArrowForwardIos } from "react-icons/md";
 import { IoMdSettings } from "react-icons/io";
 import { FaUser } from "react-icons/fa";
 
@@ -42,23 +42,32 @@ const Navigation = () => {
       <div className="px-3 pb-2 pt-6">
         <BackHome />
         <div className={`text-center pt-4`}>
-          <MdOutlineArrowBackIos onClick={()=>setShowNavbar(!showNavBar)} size={24} 
-            className={`text-white/80 hover:text-white/40 cursor-pointer 
-            ${showNavBar?"":"rotate-180"}`}
-          />
+          {showNavBar?(
+            <MdOutlineArrowBackIos onClick={()=>setShowNavbar(!showNavBar)} size={24} 
+              className="text-white/80 hover:text-white/40 cursor-pointer transition-all duration-500"
+            />
+          ):(
+            <MdOutlineArrowForwardIos onClick={()=>setShowNavbar(!showNavBar)} size={24} 
+              className="text-white/80 hover:text-white/40 cursor-pointer transition-all duration-500"
+            />
+          )}
+          
         </div>
       </div>
       
       <div className={`flex-1 overflow-y-auto overflow-x-hidden scrollbar-hidden px-3 transition ${showNavBar?"":"hidden"}`}>
         <div className="flex flex-col gap-2 ">
-          <input type="text" placeholder="Search Items . . . " className="text-[1.15rem] px-3 py-3 text-white/65 bg-transparent border-solid rounded-full focus:outline-none"/>
+          <input type="text" placeholder="Search Items . . . " className="text-[1.15rem] px-3 py-3 text-[#bbc9ee] bg-transparent border-solid rounded-full focus:outline-none"/>
           <div className="grid grid-cols-2 gap-1.5">
-            <input type="text" placeholder="Max" className="text-[1.1rem] px-3 py-1.5 text-white/65 bg-transparent border-solid rounded-lg focus:outline-none"/>
-            <input type="text" placeholder="Min" className="text-[1.1rem] px-3 py-1.5 text-white/65 bg-transparent border-solid rounded-lg focus:outline-none"/>
+            <input type="text" placeholder="Max" className="text-[1.1rem] px-3 py-1.5 text-[#bbc9ee] bg-transparent border-solid rounded-lg focus:outline-none"/>
+            <input type="text" placeholder="Min" className="text-[1.1rem] px-3 py-1.5 text-[#bbc9ee] bg-transparent border-solid rounded-lg focus:outline-none"/>
           </div>
-          <div>
-            <h2 className="text-center text-[#bbc9ee]">Categories</h2>
+          
+          <div  className="relative">
+            <div className="h-px bg-[#bbc9ee]/90 w-full absolute top-1/2 -translate-y-1/2"/>
+            <h2 className="relative text-center text-[#bbc9ee] bg-[#1A1C1E] px-3 w-fit mx-auto">Categories</h2>
           </div>
+
           {categories.map(cat => (
             <label key={cat} className="flex items-center justify-between px-3 py-2 rounded-md cursor-pointer hover:bg-white/5 transition">
               <span className="text-[1.2rem] text-white/75 capitalize">
@@ -69,17 +78,23 @@ const Navigation = () => {
                 type="radio"
                 name="category"
                 value={cat}
-                className="accent-blue-400 cursor-pointer"
+                className="accent-[#bbc9ee] cursor-pointer"
               />
             </label>
           ))}
-          <div>
-            <h2 className="text-center text-[#bbc9ee]">Keywords</h2>
+          <div  className="relative">
+            <div className="h-px bg-[#bbc9ee]/90 w-full absolute top-1/2 -translate-y-1/2"/>
+            <h2 className="relative text-center text-[#bbc9ee] bg-[#1A1C1E] px-3 w-fit mx-auto">Keywords</h2>
           </div>
           {keyword.map((word)=>(
             <button key={word} className="font-mono text-xs md:text-sm bg-white/5 hover:bg-white/10 rounded-md py-2 px-3 border-solid border-white/10 transition text-white cursor-pointer">{word}</button>
           ))}
-          <button className="font-mono mt-3 text-sm text-semibold bg-black/50 hover:bg-black/10 rounded-md py-3 px-3 border-solid border-white/30 transition text-white cursor-pointer">Reset Filters</button>
+
+          <div className="relative my-4">
+            <div className="h-px bg-[#bbc9ee] w-full absolute top-1/2 -translate-y-1/2"/>
+          </div>
+
+          <button className="font-mono mt-1 text-sm text-semibold bg-black/50 hover:bg-black/10 rounded-md py-3 px-3 border-solid border-white/30 transition text-white cursor-pointer">Reset Filters</button>
         </div>
       </div>
       <div className={`px-3 py-4 flex flex-col items-center gap-4`}>
