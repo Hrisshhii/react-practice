93b5de0b1d0d77a9bf4cbfef49b46da800b10991
Hrishi <rathodhrishi12345@gmail.com>
2026-02-15 23:05:03 +0530

    Creating context

--- ts-miniprojects/src/Components/Ecommerce Filtering Project/EcomFiltering.tsx
+++ ts-miniprojects/src/Components/Ecommerce Filtering Project/EcomFiltering.tsx
@@ -1,24 +1,27 @@
 import { useState } from "react";
 import Navigation from "./Navigation";
+import { FilterProvider } from "./FilterContext";
 
 const EcomFiltering = () => {
   const [showNavBar, setShowNavbar] = useState<boolean>(false);
 
   return (
-    <div className="relative min-h-screen bg-[#121212] overflow-hidden ml-16">
-      <Navigation showNavBar={showNavBar} setShowNavbar={setShowNavbar} />
-      {showNavBar && (
-        <div
-          onClick={() => setShowNavbar(false)}
-          className="fixed inset-0 bg-black/80 backdrop-blur-md z-40 transition-all duration-300"
-        />
-      )}
+    <FilterProvider>
+      <div className="relative min-h-screen bg-[#121212] overflow-hidden ml-16">
+        <Navigation showNavBar={showNavBar} setShowNavbar={setShowNavbar} />
+        {showNavBar && (
+          <div
+            onClick={() => setShowNavbar(false)}
+            className="fixed inset-0 bg-black/80 backdrop-blur-md z-40 transition-all duration-300"
+          />
+        )}
 
-      <div className="relative z-0 p-6 text-white">
-        <h1>Main Page Content</h1>
+        <div className="relative z-0 p-6 text-white">
+          <h1>Main Page Content</h1>
+        </div>
+
       </div>
-
-    </div>
+    </FilterProvider>
   );
 };
 
