8cfb73711eb2187e1803f129649a5f43d993595f
Hrishi <rathodhrishi12345@gmail.com>
2026-03-01 22:24:53 +0530

    Sound reactive draft 2

--- framermotion/src/components/SoundReactive/SoundReactive.tsx
+++ framermotion/src/components/SoundReactive/SoundReactive.tsx
@@ -1,9 +1,13 @@
 import { useEffect, useRef, useState } from "react";
 import { BackHome } from "../BackHome";
 import music from "../../assets/sounds/music.mp3";
+import {motion} from "framer-motion";
+import { PlusCircle } from "lucide-react";
 
 const SoundReactive = () => {
   const [isPlaying,setIsPlaying]=useState(false);
+  const bassRef = useRef(0);
+  const [bassLevel,setBassLevel]=useState(0);
   const barsRef = useRef<(HTMLDivElement | null)[]>([]);
   const audioRef=useRef<HTMLAudioElement|null>(null);
   const audioContextRef=useRef<AudioContext|null>(null);
@@ -15,19 +19,27 @@ const SoundReactive = () => {
     audioRef.current.loop=true;
   },[])
 
-  const startAudio=async ()=>{
-    if(!audioRef.current) return;
-    if(!audioContextRef.current){
-      audioContextRef.current=new AudioContext();
-      analyserRef.current=audioContextRef.current.createAnalyser();
-      analyserRef.current.fftSize=64;
+  const setupAudioContext=()=>{
+    if (!audioRef.current) return;
+    if(audioContextRef.current) return;
 
-      sourceRef.current=audioContextRef.current.createMediaElementSource(audioRef.current);
-      sourceRef.current.connect(analyserRef.current);
-      analyserRef.current.connect(audioContextRef.current.destination);
+    audioContextRef.current=new AudioContext();
+    analyserRef.current=audioContextRef.current.createAnalyser();
+    analyserRef.current.fftSize = 64;
+
+    sourceRef.current=audioContextRef.current.createMediaElementSource(audioRef.current);
+
+    sourceRef.current.connect(analyserRef.current);
+    analyserRef.current.connect(audioContextRef.current.destination);
+  };
+
+  const startAudio=async ()=>{
+    if (!audioRef.current) return;
+    setupAudioContext();
+    if(audioContextRef.current?.state==="suspended"){
+      await audioContextRef.current?.resume();
     }
-    await audioContextRef.current.resume();
-    audioRef.current.play();
+    await audioRef.current.play();
     setIsPlaying(true);
     animateBars();
   };
@@ -38,39 +50,87 @@ const SoundReactive = () => {
     const dataArray=new Uint8Array(bufferLen);
     const update=()=>{
       analyserRef.current?.getByteFrequencyData(dataArray);
+      const bass=dataArray.slice(0,5).reduce((a,b)=>a+b,0)/5;
+      const normalizedBass=bass/255;
 
       barsRef.current.forEach((bar,i)=>{
         const value=dataArray[i];
         const scale=value/255;
         if(bar){
           bar.style.transform=`scaleY(${Math.max(scale,0.1)})`;
+          bar.style.transition="transform 0.08s ease-out";
         }
       });
       requestAnimationFrame(update);
+      bassRef.current = normalizedBass;
     };
     update();
   };
 
+  useEffect(() => {
+  const interval = setInterval(() => {
+    setBassLevel(bassRef.current);
+  }, 50);
+
+  return () => clearInterval(interval);
+}, []);
+
   return (
     <div className="relative h-screen bg-black overflow-hidden">
       <BackHome />
-      <div className="flex flex-cols justify-center items-center h-screen gap-10">
-        <input type="file" accept="audio/*" onChange={(e)=>{
-          const file=e.target.files?.[0];
-          if(file){
-            audioRef.current=new Audio(URL.createObjectURL(file));
-          }
-        }} />
-        <button onClick={()=>{startAudio()}} className="px-6 py-3 bg-purple-600 text-white rounded-lg">
-          {isPlaying?"Playing...":"Play Music"}
-        </button>
-        <div className="flex items-end gap-2 h-40">
-          {Array.from({length:32}).map((_,i)=>(
-            <div key={i} className="w-2 bg-linear-to-t from-purple-500 to-pink-500 rounded"
-            ref={(el)=>{barsRef.current[i]=el!}}
-            style={{height:"100%"}}
+      <motion.div className="absolute inset-0 bg-purple-900 blur-3xl -z-10"
+        animate={{opacity: 0.2+bassLevel*0.5}}
+      />
+      <div className="flex items-center justify-center h-screen">
+        <div className="backdrop-blur-xl bg-white/5 border border-white/10 p-6 rounded-2xl flex flex-col items-center gap-4 shadow-xl">
+          <label>
+            <PlusCircle className="text-white cursor-pointer"/>
+            <input
+              type="file"
+              accept="audio/*"
+              className="hidden"
+              onChange={(e) => {
+                const file=e.target.files?.[0];
+                if (!file) return;
+
+                if (audioRef.current) {
+                  audioRef.current.pause();
+                }
+                if(audioContextRef.current){
+                  audioContextRef.current.close();
+                  audioContextRef.current=null;
+                  analyserRef.current=null;
+                  sourceRef.current=null;
+                }
+                const url=URL.createObjectURL(file);
+                audioRef.current=new Audio(url);
+                audioRef.current.loop=true;
+                setIsPlaying(false);
+              }}
             />
-          ))}
+          </label>
+          
+          <button onClick={async ()=>{
+            if(!audioRef.current) return;
+            if(isPlaying){
+              audioRef.current.pause();
+              setIsPlaying(false);
+            }else{
+              startAudio();
+            }
+            }} 
+            className="px-6 py-3 bg-purple-600 text-white rounded-lg cursor-pointer"
+          >
+            {isPlaying ? "Pause":"Play"}
+          </button>
+          <div className="flex items-end gap-2 h-40">
+            {Array.from({length:32}).map((_,i)=>(
+              <div key={i} className="w-2 bg-linear-to-t from-purple-500 to-pink-500 rounded"
+              ref={(el)=>{barsRef.current[i]=el!}}
+              style={{height:"100%"}}
+              />
+            ))}
+          </div>
         </div>
       </div>
     </div>
