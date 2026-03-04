988393893092d2a1af591f9d23333bf738f88d71
Hrishi <rathodhrishi12345@gmail.com>
2026-03-04 19:41:14 +0530

    Adding animation to description

--- framermotion/src/components/Morphing/GlassMorph.tsx
+++ framermotion/src/components/Morphing/GlassMorph.tsx
@@ -57,7 +57,8 @@ const GlassMorph=()=>{
         />
         <div className="relative z-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10">
           {cards.map((card)=>(
-            <motion.div key={card.id} layoutId={`card-${card.id}`} whileHover={{scale:1.05,y:-10,boxShadow:"0px 20px 80px rgba(168,85,247,0.4)"}} onClick={()=>setSelected(card)}
+            <motion.div key={card.id} layoutId={`card-${card.id}`} whileHover={{scale:1.05,y:-10,boxShadow:"0px 20px 80px rgba(168,85,247,0.4)"}}
+              transition={{type:"spring",stiffness:200,damping:20}}  onClick={()=>setSelected(card)}
               className="cursor-pointer p-6 w-72 h-50 bg-white/5 backdrop-blur-xl border border-white/10 rounded-2xl shadow-[0_0_40px_rgba(168,85,247,0.2)] text-white flex justify-center items-center"
             >
               <motion.h2 layoutId={`title-${card.id}`} className="text-xl font-semibold">{card.title}</motion.h2>
@@ -75,7 +76,7 @@ const GlassMorph=()=>{
                 className="absolute z-20 p-10 w-150 h-100 max-w-[90%] bg-white/10 backdrop-blur-2xl border border-white/20 rounded-3xl shadow-[0_0_80px_rgba(168,85,247,0.4)] text-white flex flex-col"
               >
                 <motion.h2 layoutId={`title-${selected.id}`} className="text-3xl font-bold">{selected.title}</motion.h2>
-                <motion.p layoutId={`desc-${selected.id}`} className="mt-4 text-white/60">{selected.description}</motion.p>
+                <motion.p initial={{opacity:0}} animate={{opacity:1}} transition={{delay:0.5}} layoutId={`desc-${selected.id}`} className="mt-4 text-white/60">{selected.description}</motion.p>
                 <button onClick={()=>setSelected(null)} className="mt-auto px-6 py-2 bg-white/10 rounded-xl hover:bg-white/20 transition cursor-pointer">Close</button>
               </motion.div>
             </>
