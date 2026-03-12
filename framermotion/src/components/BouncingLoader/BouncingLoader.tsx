355f84f19463ed84526905bb412df87d3588b568
Hrishi <rathodhrishi12345@gmail.com>
2026-03-12 23:32:43 +0530

    Bouncing loader version one

--- framermotion/src/components/BouncingLoader/BouncingLoader.tsx
+++ framermotion/src/components/BouncingLoader/BouncingLoader.tsx
@@ -1,10 +1,23 @@
 import { BackHome } from "../BackHome"
-
+import {motion} from "framer-motion";
 const BouncingLoader = () => {
   return (
     <div className="relative h-screen bg-black overflow-hidden">
       <div className="relative z-50">
         <BackHome />
+      </div>
+      <div className="absolute inset-0 flex justify-center items-center">
+        {[...Array(3)].map((_,index)=>(
+          <motion.div key={index} className="m-1 w-4 h-4 bg-teal-500 rounded-full"
+            animate={{y:[0,-30,0]}}
+            transition={{
+              duration:0.6,
+              ease:"easeInOut",
+              repeat:Infinity,
+              repeatDelay: index * 0.2,
+            }}
+          />
+        ))}
       </div>
     </div>
   )
