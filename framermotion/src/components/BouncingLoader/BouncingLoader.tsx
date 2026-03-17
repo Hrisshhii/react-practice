f5c6ccd89840417dac310acec7ba73033c08c053
Hrishi <rathodhrishi12345@gmail.com>
2026-03-17 20:13:20 +0530

    Adding header

--- framermotion/src/components/BouncingLoader/BouncingLoader.tsx
+++ framermotion/src/components/BouncingLoader/BouncingLoader.tsx
@@ -1,11 +1,14 @@
+
 import { BackHome } from "../BackHome"
 import {motion} from "framer-motion";
+
 const BouncingLoader = () => {
   return (
     <div className="relative h-screen bg-black overflow-hidden">
       <div className="relative z-50">
         <BackHome />
       </div>
+      <h1 className="text-center text-5xl font-bold bg-clip-text bg-linear-to-r from-white to-blue-400 text-transparent tracking-wide">Loader Animation</h1>
       <div className="absolute inset-0 flex justify-center items-center">
         <motion.div className="absolute w-40 h-40 rounded-full bg-teal-500/20 blur-3xl"
           animate={{
