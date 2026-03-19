00cc0d7f8a786a546070a14baf6d83b7cb395cbd
Hrishi <rathodhrishi12345@gmail.com>
2026-03-19 21:34:30 +0530

    Adding heading and Backhome button the gestures miniproject

--- framermotion/src/components/Gesture/Gesture.tsx
+++ framermotion/src/components/Gesture/Gesture.tsx
@@ -1,8 +1,14 @@
+import { BackHome } from "../BackHome"
 
 
 const Gesture = () => {
   return (
-    <div></div>
+    <div className="relative h-screen bg-black overflow-hidden">
+      <div className="relative z-50">
+        <BackHome/>
+      </div>
+      <h1 className="text-center text-5xl font-bold bg-clip-text bg-linear-to-r from-white to-blue-400 text-transparent tracking-wide">Gestures</h1>
+    </div>
   )
 }
 
